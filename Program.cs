1342092ba5639be6e8707e0fc25c5b2b8840ba39
carter-oleary <carter.m.oleary@outlook.com>
2025-04-21 11:22:16 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] s = { "This is a test string", "Some   weird spacingin   this       one" };
+
+foreach (string test in s)
+{
+    Console.WriteLine($"The string \"{test}\" has {GetNumberOfSpaces(test)} spaces.");
+}
+
+int GetNumberOfSpaces(string s)
+{
+    int numSpaces = 0;
+    foreach(char c in s)
+    {
+        if (c == ' ') { numSpaces++; }
+    }
+    return numSpaces;
+}
+
